9dca2879957f6b8ba9aa4d60279a8ba057f8a2c3
senorcl0ud <claudemcalpin@gmail.com>
2017-10-18 20:38:28 -0400

    OH MY GOD THIS TOOK SO LONG AND I NEED HELP

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,54 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+        System.out.println("Choose a whole number between the number 0 and 9.");
+
+        int randomInt = (int) Math.floor(Math.random() * 10);
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        int guessCount = 0;
+        int currentGuess;
+        int lastGuess=-1;
+
+
+        do {
+
+            currentGuess = scanner.nextInt();
+
+            if (currentGuess > randomInt) {
+                System.out.println("Too large!");
+
+            } else if (currentGuess < randomInt) {
+                System.out.println("Too small! ");
+
+            }
+
+            if (currentGuess != lastGuess) {
+                guessCount++;
+            }
+            System.out.println("Guess again.");
+
+            lastGuess = currentGuess;
+
+
+
+
+        }
+        while (currentGuess != randomInt);
+
+
+        System.out.println("Correct! Well done.");
+        System.out.println("You made " + guessCount + " guesses...lol");
+
     }
+
+
 }
+
