819026eddc32f4bdb3a905a0cd3c17528afaebeb
andrea <andrea@zipcoders-MacBook-Pro-2.local>
2017-10-28 21:18:18 -0400

    I re-did this lab too because I am smarter now.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,34 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+//    Write a guessing game which prompts a user to guess a mystery number within some range.
+//    After every guess the program should display some variation of "too large", "too small", "correct guess", respectively.
+//    Upon termination, the program should display the number of guesses before successfully guessing correctly.
+//    A number that is input consecutively, should register as a single guess.
 
     public static void main(String[] args) {
-        int randomNumber = (int) (Math.random() * 10);
+        int computerNumber;
+        computerNumber = (int) (Math.random() * 9 + 1);
+//        System.out.println(computerNumber);
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Guess a number between 1 and 10");
-        System.out.println(randomNumber);
-        int userNumber;
+        int userGuess;
 
-        do {
-            userNumber = scanner.nextInt();
-            if (randomNumber > userNumber) {
-                System.out.println("too small");
-            } else if (randomNumber < userNumber) {
-                System.out.println("too large");
-            } else {
-                System.out.println("correct guess");
+        do{
+            System.out.println("Play my guessing game! Pick a number from 1 to 10.");
+            userGuess = scanner.nextInt();
+            System.out.println("Your guess is: " + userGuess + ".");
+            if (userGuess == computerNumber){
+                System.out.println("Correct guess! You win!");
+                break;
+            }else if(userGuess > computerNumber){
+                System.out.println("Sorry, your number is too large. Guess again.");
+            }else if(userGuess < computerNumber){
+                System.out.println("Sorry, your number is too small. Guess again.");
             }
-
-        } while (userNumber != randomNumber);
+        } while (userGuess != computerNumber);
     }
 }
