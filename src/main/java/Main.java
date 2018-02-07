75cbe8a3e2a2eb005d9201b48a57ed1bddd6c369
Brian He <brianhe@zipcoders-MacBook-Pro-56.local>
2018-02-07 13:28:27 -0500

    Revisions

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,25 +4,31 @@ public class Main {
     public static void main(String[] args){
 
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter a whole number: ");
-        int input = scan.nextInt();
 
         Random rando = new Random();
-        int randNum = rando.nextInt(input + 1);
+        int randNum = rando.nextInt(10);
 
-        System.out.println("Make your guess");
+        System.out.println("Guess a number between 0 and 10: ");
         int guess = scan.nextInt();
         int count = 0;
+        int current = 0;
+
         while(guess != randNum){
-            if(guess > randNum) {
+            if(guess > randNum && guess != current) {
                 System.out.println("Too large");
-            } else {
+                current = guess;
+            } else if(guess < randNum && guess != current){
                 System.out.println("Too small");
+                current = guess;
+            } else if(guess == current) {
+                System.out.println("You just guessed this");
+                count--;
             }
             System.out.println("Guess again");
             guess = scan.nextInt();
             count++;
         }
-        System.out.println("You got it! Only took you " + count + " tries.");
+        count++;
+        System.out.println("You got it! Only took you (" + count + ") tries.");
     }
 }
