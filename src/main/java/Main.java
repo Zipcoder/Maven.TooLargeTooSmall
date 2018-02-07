3e0648f2c673864f417aaf04a6839cc76f139c04
Carolynn Vansant <cvansant@MacBook-Pro-5.local>
2018-02-07 14:57:49 -0500

    second commit small fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,25 +6,25 @@ public class Main {
 
 
     public static void main(String[] args){
-        Scanner nin = new Scanner(System.in);
+        Scanner userIn = new Scanner(System.in);
         System.out.println("Please enter an integer between 0 and 10");
-        int usein = nin.nextInt();
+        int userNumber = userIn.nextInt();
         int random = (int)(Math.random() * 10);
         int count = 1;
             do {
-                if (usein > random) {
+                if (userNumber > random) {
                     System.out.println("too large");
-                } else if (usein < random) {
+                } else if (userNumber < random) {
                     System.out.println("too small");
                 } else {
                     System.out.println("invalid entry");
                 }
                 Scanner retry = new Scanner(System.in);
                 System.out.println("Enter a new number");
-                usein = retry.nextInt();
+                userNumber = retry.nextInt();
                 count++;
-            } while (usein != random);
-            if(usein == random) {
+            } while (userNumber != random);
+            if(userNumber == random) {
                 System.out.println("correct guess");
             }
 
