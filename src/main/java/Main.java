fed03931113a51149ca30da8946cb9c5d08dbd8d
Keith Brinker <keithbrinker@zipcoders-MacBook-Pro-2.local>
2018-02-07 17:01:45 -0500

    GuessingGame Lab

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,39 @@
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
+
+import java.util.Scanner;
+import java.util.Random;
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
-        
+        Random rand = new Random();
+        int correctnum = rand.nextInt(10) + 1;
+
+        int givennumber = Integer.MIN_VALUE;
+        int attemps = 0;
+
+        Scanner number = new Scanner(System.in);
+        System.out.println("Pick a number between 1 and 10");
+        while (givennumber != correctnum) {
+            String answer = number.nextLine();
+            givennumber = Integer.parseInt(answer);
+            System.out.println("You guessed " + answer);
+
+
+
+            if (givennumber == correctnum) {
+                System.out.println("You win!");
+            } else if (givennumber >= correctnum) {
+                System.out.println("Too big, guess again");
+                ++attemps;
+            } else if (givennumber <= correctnum) {
+                System.out.println("Too Small, guess again");
+                ++attemps;
+            }
+        }
+        System.out.println("The correct answer was " +  correctnum);
+        System.out.println("You guessed " + attemps +" time(s)");
     }
 }
