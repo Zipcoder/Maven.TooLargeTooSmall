a9d964135ae4b1372ad6c8c7495d3c8067c562f4
Brian He <brianhe@zipcoders-MacBook-Pro-56.local>
2018-02-07 11:13:50 -0500

    Random number

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,28 @@
-/**
- * Created by iyasuwatts on 10/17/17.
- */
-public class Main {
+import java.util.*;
 
+public class Main {
     public static void main(String[] args){
-        
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a whole number: ");
+        int input = scan.nextInt();
+
+        Random rando = new Random();
+        int randNum = rando.nextInt(input + 1);
+
+        System.out.println("Make your guess");
+        int guess = scan.nextInt();
+        int count = 0;
+        while(guess != randNum){
+            if(guess > randNum) {
+                System.out.println("Too large");
+            } else {
+                System.out.println("Too small");
+            }
+            System.out.println("Guess again");
+            guess = scan.nextInt();
+            count++;
+        }
+        System.out.println("You got it! Only took you " + count + " tries.");
     }
 }
