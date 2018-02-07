6b05eee66f376ae657d59f147b28811093b7a057
Haysel Santiago <hayselsantiago@zipcoders-MacBook-Pro-7.local>
2018-02-07 16:14:05 -0500

    TooLargeTooSmall Finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,36 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
-public class Main {
+import java.util.Random;
+import java.util.Scanner;
 
-    public static void main(String[] args){
-        
+public class main {
+
+    public static void main(String[] args) {
+
+        Random rand = new Random();
+        int numberToGuess = rand.nextInt(500);
+        int numberOfTries = 0;
+        Scanner input = new Scanner(System.in);
+        int guess;
+        boolean win = false;
+
+        while (!win) {
+
+            System.out.println("Guess a number between 1 and 500: ");
+            guess = input.nextInt();
+            numberOfTries++;
+
+            if (guess == numberToGuess) {
+                win = true;
+            } else if (guess < numberToGuess) {
+                System.out.println("Too small!");
+            } else if (guess > numberToGuess) {
+                System.out.println("Too large!");
+            }
+        }
+        System.out.println("You win!");
+        System.out.println("The number was " + numberToGuess);
+        System.out.println("It took you " + numberOfTries + " tries");
     }
 }
