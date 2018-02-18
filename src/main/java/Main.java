86817d66a7491cace9f5885257447c741be96b8a
Amy Gill <amygill@MacBook-Pro-2.local>
2018-02-18 18:53:12 -0500

    redone. better understanding of java util Random class. still working on the details loop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,53 @@ import java.util.Random;
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a number between 1 and 10.");
+
+        Random generator = new Random();
+        int min = 1;
+        int max = 10;
+        int randomlyGeneratedNumber = min + generator.nextInt(max);
+
+        boolean didTheyGuessCorrectly = false;
+        int count = 0;
+
+        while (!didTheyGuessCorrectly) {
+            int usersGuess = input.nextInt();
+            count++;
+            if (usersGuess > randomlyGeneratedNumber) {
+                System.out.println("Sorry. Your guess was too big. Please try again.");
+            } else if (usersGuess < randomlyGeneratedNumber) {
+                System.out.println("Sorry. Your guess was too small. Please try again.");
+            } else if (usersGuess == randomlyGeneratedNumber){
+                System.out.println("Congratulations! You are correct. It took you exactly " + count + " tries." +
+                        "\nHave a nice day.");
+                didTheyGuessCorrectly = true;
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 public class Main {
 
     public static void main(String[] args) {
@@ -50,5 +97,4 @@ public class Main {
 
         System.out.println(count);
 */
-    }
-}
+
