8270f952ad3f9c6d942a8f0bfbfa50e86a0dbc41
Kthomas <kthomas@github.com>
2018-02-27 13:44:43 -0500

    Cleaner

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,22 +2,15 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class Main {
-
     public static void main(String[] args) {
         Random generator = new Random();
         Scanner reader = new Scanner(System.in);
         int solution = generator.nextInt(100);
         int tryCounter = 0;
-
         System.out.print("What number am I thinking of?...\n ");
-
         int input = reader.nextInt();
-
     while (input != solution){
-
-
          if (input > solution) {
-
                 System.out.print("Thats too high try again\n> ");
                 input = reader.nextInt();
             }
@@ -31,24 +24,3 @@ public class Main {
     }
          // System.out.println(tryCounter);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
